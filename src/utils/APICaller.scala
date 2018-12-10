34d9c203cd6c116ea3045e04bbc87954108d231c
alelib94 <alelib1994@gmail.com>
2018-12-10 12:58:34 +0100

    Added token access to files

--- src/utils/APICaller.scala
+++ src/utils/APICaller.scala
@@ -6,11 +6,14 @@ class APICaller {
 
   val baseURL:String = "https://mastodon.social"
   val endpoint:String = "/api/v1/streaming/local"
-  val token:String = "829f7ee5d29f3cc15ef545b4bdfb0ea3dabcce01038c3f1ca5b36703749f883e"
+  val token:String = "9ae77419135fc815ec25c3d16b4ed9ca1cf440fa3119c0fa9f68e4341df5ff46"
+  val client_id:String = "170d143404860f0300cec84848602237dd5a858b22859d1a300ed0fb31273d40"
+  val secret:String = "a74cf401511d7c631b75a92430e260880799520ca57d304c019a3bc538f5ea89"
 
   def manageStream() = {
     val request = Http(baseURL + endpoint)
-    request.headers(("bearer_token", token))
+   // request.headers("client_id", )
+    request.headers(("Authorization", token))
     request.execute(is => {
       scala.io.Source.fromInputStream(is).getLines.foreach(println)
     })
