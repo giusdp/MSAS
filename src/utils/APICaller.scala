eeba708083afd4c898ad83e561f3246eba8e610d
alelib94 <alelib1994@gmail.com>
2018-12-10 12:34:41 +0100

    Created test account

--- src/utils/APICaller.scala
+++ src/utils/APICaller.scala
@@ -6,12 +6,14 @@ class APICaller {
 
   val baseURL:String = "https://mastodon.social"
   val endpoint:String = "/api/v1/streaming/local"
+  val token:String = "829f7ee5d29f3cc15ef545b4bdfb0ea3dabcce01038c3f1ca5b36703749f883e"
 
   def manageStream() = {
-    val response = Http(baseURL + endpoint).execute(is => {
+    val request = Http(baseURL + endpoint)
+    request.headers(("bearer_token", token))
+    request.execute(is => {
       scala.io.Source.fromInputStream(is).getLines.foreach(println)
     })
-
   }
 
 }
