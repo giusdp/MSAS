f251b96b23c27f6fd7f41584739bc7519a6d5cbe
Giuseppe De Palma <depalma.gsp@gmail.com>
2018-12-10 14:02:17 +0100

    Ignoring exception to keep connection alive

--- src/utils/APICaller.scala
+++ src/utils/APICaller.scala
@@ -1,17 +1,27 @@
 package utils
 
+import java.net.SocketTimeoutException
+
 import scalaj.http.{Http, HttpResponse}
 
 class APICaller {
 
-  val baseURL: String = "https://mastodon.technology"
+  val baseURL: String = "https://mastodon.xyz"
   val endpoint: String = "/api/v1/streaming/public"
 
-  def manageStream(): HttpResponse[Unit] = {
-    val request = Http(baseURL + endpoint)
-    request.execute(is => {
-      scala.io.Source.fromInputStream(is).getLines.foreach(println)
-    })
+  def manageStream(): Unit = {
+    while (true) {
+      try {
+        val request = Http(baseURL + endpoint)
+        request.execute(is => {
+          scala.io.Source.fromInputStream(is).getLines.foreach(println)
+        })
+      } catch {
+        case e: SocketTimeoutException => println(e.getMessage)
+        case e: NullPointerException => println(e.getMessage)
+      }
+    }
+    println("Finito")
   }
 
 }
